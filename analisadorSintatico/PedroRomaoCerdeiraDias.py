31f1bb38f579995926dc6b98cd02150d0941f6c7
Pedro Romão <pedro.dias@sou.inteli.edu.br>
2023-11-25 14:36:17 -0300

    final

--- analisadorSintatico/PedroRomaoCerdeiraDias.py
+++ analisadorSintatico/PedroRomaoCerdeiraDias.py
@@ -419,33 +419,18 @@ class AnalisadorSintatico:
 			sinal = self.comparar("OPSUM").valor
 			factor = self.factor()
 		elif self.tokenCorrente.tipo == "NUMBER":
-			valor = self.comparar("NUMBER").valor
-			factor = NoFolha(op='num', valor=valor, linha=self.tokenCorrente.linha)
+			num = self.comparar("NUMBER")
+			factor = NoFolha(op='num', valor=num.valor, linha=self.tokenCorrente.linha)
 		elif self.tokenCorrente.tipo == "BOOLEAN":
-			valor = self.comparar("BOOLEAN").valor
-			factor = NoFolha(op='log', valor=valor, linha=self.tokenCorrente.linha)
+			bool = self.comparar("BOOLEAN")
+			factor = NoFolha(op='log', valor=bool.valor, linha=self.tokenCorrente.linha)
 		elif self.tokenCorrente.tipo == "ID":
-			valor = self.comparar("ID").valor
-			factor = NoFolha(op='id', valor=valor, linha=self.tokenCorrente.linha)
+			id = self.comparar("ID")
+			factor = NoFolha(op='id', valor=id.valor, linha=self.tokenCorrente.linha)
 		elif self.tokenCorrente.tipo == "LPAR":
 			self.comparar("LPAR")
 			factor = self.expression()
 			self.comparar("RPAR")
 
 		return NoInterno(op="factor", sinal=sinal, esq=None, dir=None, factor=factor)
-		
-			
-if __name__ == '__main__':
-	tokens = [Token("ALG", "alg", 1), Token("ID", "testa_expressoes", 1), Token("VAR", "var", 2), Token("TYPE", "num", 3), Token("ID", "x", 3), Token("COMMA", ",", 3),
-		Token("ID", "y", 3), Token("SEMICOLON", ";", 3), Token("LBLOCK", "{", 4), Token("ID", "x", 5), Token("ASSIGN", "=", 5), Token("LPAR", "(", 5), Token("NUMBER", "16", 5),
-		Token("OPMUL", "#", 5), Token("NUMBER", "10", 5), Token("RPAR", ")", 5), Token("OPSUM", "+", 5), Token("NUMBER", "3", 5), Token("OPMUL", ".", 5), Token("LPAR", "(", 5),
-		Token("NUMBER", "27", 5), Token("OPMUL", ":", 5), Token("NUMBER", "9", 5), Token("RPAR", ")", 5), Token("SEMICOLON", ";", 5), Token("ID", "y", 6), Token("ASSIGN", "=", 6),
-		Token("NUMBER", "2", 6), Token("OPPOW", "^", 6), Token("NUMBER", "3", 6), Token("OPPOW", "^", 6), Token("NUMBER", "4", 6), Token("OPSUM", "-", 6), Token("ID", "x", 6),
-		Token("SEMICOLON", ";", 6), Token("IF", "if", 7), Token("ID", "x", 7), Token("OPREL", ">", 7), Token("ID", "y", 7), Token("LBLOCK", "{", 7), Token("OUT", "out", 8),
-		Token("LPAR", "(", 8), Token("ID", "x", 8), Token("RPAR", ")", 8), Token("SEMICOLON", ";", 8), Token("RBLOCK", "}", 9), Token("ELSE", "else", 9), Token("LBLOCK", "{", 9),
-		Token("IF", "if", 10), Token("ID", "x", 10), Token("OPREL", "==", 10), Token("ID", "y", 10), Token("LBLOCK", "{", 10), Token("OUT", "out", 11), Token("LPAR", "(", 11),
-		Token("ID", "x", 11), Token("RPAR", ")", 11), Token("SEMICOLON", ";", 11), Token("OUT", "out", 12), Token("LPAR", "(", 12), Token("ID", "y", 12), Token("RPAR", ")", 12),
-		Token("SEMICOLON", ";", 12), Token("RBLOCK", "}", 13), Token("ELSE", "else", 13), Token("LBLOCK", "{", 13), Token("OUT", "out", 14), Token("LPAR", "(", 14), Token("ID", "y", 14),
-		Token("RPAR", ")", 14), Token("SEMICOLON", ";", 14), Token("RBLOCK", "}", 15), Token("RBLOCK", "}", 16), Token("RBLOCK", "}", 17), Token("EOF", "EOF", 17)]
-	sintatico = AnalisadorSintatico(tokens)
-	print(sintatico.analisar())
+	
